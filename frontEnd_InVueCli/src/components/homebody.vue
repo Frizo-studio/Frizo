671861720d758c99c71eba45e6aae231858c7e43
Shang-Ru <best0049@gmail.com>
2020-03-04 23:55:46 +0800

    'VueUpData'

---
+++ frontEnd_InVueCli/src/components/homebody.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="homebody">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-2">
+          <sidelist></sidelist>
+        </div>
+        <div class="col-sm-10">One of three columns</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sidelist from '@/components/sidelist'
+
+export default {
+  name: 'Homebody',
+  components: {
+    sidelist
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.homebody {
+  position: relative;
+  left: 80px;
+  top: 15px;
+  width: 65vw;
+  height: 600px;
+}
+</style>
---
+++ frontEnd_InVueCli/src/pages/homepage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <homebody></homebody>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar'
+import homebody from '@/components/homebody'
+
+export default {
+  name: 'Homepage',
+  components: {
+    navbar,
+    homebody
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  font-family: Malgun Gothic;
+}
+</style>
